f6cab1a168cfdd377223f86a4c5226455378a7b7
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2018-08-28 10:31:01 -0700

    added 'lazy fetching'

--- schema.js
+++ schema.js
@@ -17,11 +17,11 @@ const BookType = new GraphQLObjectType({
    fields: () => ({
     title: {
       type: GraphQLString,
-      resolve: xml => xml.title[0]
+      resolve: xml => xml.GoodreadsResponse.book[0].title[0]
     },
     isbn: {
       type: GraphQLString,
-      resolve: xml => xml.isbn[0]
+      resolve: xml => xml.GoodreadsResponse.book[0].isbn[0]
     }
   })
 });
@@ -37,8 +37,15 @@ const AuthorType = new GraphQLObjectType({
     },
     books: {
       type: new GraphQLList(BookType),
-      resolve: xml =>
-        xml.GoodreadsResponse.author[0].books[0].book
+      resolve: xml => {
+        const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem => elem.id[0]._)
+        console.log('fetching bookkks!')
+        return Promise.all(ids.map(id =>
+          fetch(`https://www.goodreads.com/book/show/${id}.xml?key=42tmzmwXfJJzHcbXlBRg`)
+            .then(response => response.text())
+            .then(parseXML)
+        ))
+      }   
      }
    })
 });
